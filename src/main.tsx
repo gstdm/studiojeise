2381a22a74f3304217562f9c13dc4c6a205fb7fc
gstdm <98673714+gstdm@users.noreply.github.com>
2025-01-24 20:40:36 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import Comandos from './pages/Comandos.tsx';
 import './index.css';

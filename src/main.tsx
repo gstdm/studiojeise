84b723f1d848da2e0aadfe3086d8f7a11cf333f2
gstdm <98673714+gstdm@users.noreply.github.com>
2025-01-24 20:35:37 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import Comandos from './pages/Comandos.tsx';
 import './index.css';
@@ -8,14 +7,12 @@ import ComprarPlanos from './pages/PlansGr.tsx';
 import ComprarPlanosPv from './pages/PlanosPv.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/comandos" element={<Comandos />} />
-        <Route path="/planos" element={<ComprarPlanos/>} />
+        <Route path="/planos" element={<ComprarPlanos />} />
         <Route path="/planospv" element={<ComprarPlanosPv />} />
       </Routes>
     </Router>
-  </StrictMode>
 );

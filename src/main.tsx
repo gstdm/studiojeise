ce1ffb58961c6c23b079d9aa5adf727b72236fd2
gstdm <98673714+gstdm@users.noreply.github.com>
2025-02-04 15:39:16 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
-import Comandos from './pages/Comandos.tsx';
 import './index.css';
-import ComprarPlanos from './pages/PlansGr.tsx';
-import ComprarPlanosPv from './pages/PlanosPv.tsx';
-import TermosGp from './pages/TermosGp.tsx'; // Importe a página de termos
-import TermosPv from './pages/TermosPv.tsx'; 
-import VipPage from './pages/Vip.tsx'; 
+import Modelos from './pages/Modelos.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/comandos" element={<Comandos />} />
-        <Route path="/planos" element={<ComprarPlanos />} />
-        <Route path="/planospv" element={<ComprarPlanosPv />} />
-        <Route path="/planos/termos" element={<TermosGp />} /> 
-        <Route path="/planospv/termos" element={<TermosPv />} />
-        <Route path="/gpvip" element={<VipPage />} />
+        <Route path="/modelos" element={<Modelos />} />
       </Routes>
     </Router>
 );

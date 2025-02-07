02e5d27bebb78c5d61c416674b0bb566601db2c8
gstdm <98673714+gstdm@users.noreply.github.com>
2025-02-07 20:26:56 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import Modelos from './pages/Modelos.tsx';
+import Agendamento from './pages/Agendamento.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/modelos" element={<Modelos />} />
+        <Route path="/agendamento" element={<Agendamento />} />
       </Routes>
     </Router>
 );

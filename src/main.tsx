a1659988791dada58c4a30dd464365061ac45e13
gstdm <98673714+gstdm@users.noreply.github.com>
2025-06-20 16:23:33 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from './App.tsx';
 import './index.css';
 import Modelos from './pages/Modelos.tsx';
 import Agendamento from './pages/Agendamento.tsx';
+import Admin from './pages/Admin.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/modelos" element={<Modelos />} />
+        <Route path="/admin" {<Admin />} />
         <Route path="/agendamento" element={<Agendamento />} />
       </Routes>
     </Router>

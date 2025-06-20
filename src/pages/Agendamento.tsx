a744ef9ef728d0aaeccf0c335e9c939742fe3f73
gstdm <98673714+gstdm@users.noreply.github.com>
2025-06-20 18:27:29 -0300

    Update Agendamento.tsx

--- src/pages/Agendamento.tsx
+++ src/pages/Agendamento.tsx
@@ -29,8 +29,8 @@ function Agendamento() {
             <li className="text-blue-600 text-3xl font-bold">Pré e Pós Agendamento</li>
             <li><a href="https://wa.me/558988023208" target="_blank" rel="noopener noreferrer">Agendar Horário</a></li>
             <li><a href="https://www.instagram.com/jeuusilayne.s" target="_blank" rel="noopener noreferrer">Instagram</a></li>
-            <li><a href="/#/">Tela Inicial</a></li>
-            <li><a href="/#/modelos">Ver Modelos</a></li>
+            <li><a href="/">Tela Inicial</a></li>
+            <li><a href="/modelos">Ver Modelos</a></li>
           </ul>
         </div>
       )}

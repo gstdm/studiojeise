d10d307214eb24895f3e56450e7f2c3d1a38e7f2
gstdm <98673714+gstdm@users.noreply.github.com>
2025-06-21 11:46:41 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import Modelos from './pages/Modelos.tsx';
